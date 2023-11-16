3a1b066884845b811eaa3755647ec1aa99756c17
Rodrigo Ramos <rodrigo.sr@hotmail.com>
2023-11-16 16:12:10 -0300

    Watch List (100% RSpec - No layout yet)

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,33 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: [:destroy]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :see_other
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,28 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  root to: "lists#index"
+  resources :lists do
+    member do
+      get :bookmarks
+    end
+    resources :bookmarks, only: [:new]
+  end
+  resources :bookmarks, only: [:destroy, :create]
+  post '/lists/:list_id/bookmarks', to: 'bookmarks#create'
+  # get '/lists', to: 'lists#index'
+  # get 'lists/new', to: 'lists#new'
+  # get 'lists/:id/edit', to: 'lists#edit'
+  # get 'lists/:id', to: 'lists#show'
+  # patch 'lists/:id', to: 'lists#update'
+  # delete 'lists/:id', to: 'lists#destroy'
+  # get '/bookmarks', to: 'bookmarks#index'
+  # post 'bookmarks', to: 'bookmarks#create'
+  # get '/lists/:list_id/bookmarks', to: 'bookmarks#new'
+  # get 'bookmarks/:id/edit', to: 'bookmarks#edit'
+  # get 'bookmarks/:id', to: 'bookmarks#show'
+  # patch 'bookmarks/:id', to: 'bookmarks#update'
+  # delete 'bookmarks/:id', to: 'bookmarks#destroy'
 
   # Defines the root path route ("/")
   # root "posts#index"
